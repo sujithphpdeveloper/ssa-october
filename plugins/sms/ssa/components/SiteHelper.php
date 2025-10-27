850bebd38aed0dfc5088a8049fd2a94c514fc34f
sujith <sujith@houseofcomms.com>
2025-10-27 15:15:15 +0400

    moved the tournaments list to component

--- plugins/sms/ssa/components/SiteHelper.php
+++ plugins/sms/ssa/components/SiteHelper.php
@@ -1,7 +1,10 @@
 <?php namespace SMS\SSA\Components;
 
+use Carbon\Carbon;
 use Cms\Classes\ComponentBase;
 use Flash;
+use Illuminate\Support\Facades\DB;
+use SMS\SSA\Models\Tournament;
 use Validator;
 use ValidationException;
 use SMS\SSA\Models\FormSubmission;
@@ -74,5 +77,44 @@ class SiteHelper extends ComponentBase
         } else {
             Flash::error('Sorry! Your submission has been failed!');
         }
+    }
+
+    public function getTournamentList() {
+
+        $tournamentDetails = [];
+        $today = Carbon::today()->toDateString();
+
+        $upcomingDayCount = 4;
+
+        $tournamentDetails['upcomingDays'] = Tournament::published()
+            ->selectRaw('DISTINCT DATE(date) as unique_date')
+            ->where('date', '>=', $today)
+            ->orderBy('unique_date', 'asc')
+            ->pluck('unique_date')
+            ->take($upcomingDayCount);
+
+        $pastDayCount = ($upcomingDayCount - count($tournamentDetails['upcomingDays']) == 0)? 2: (2+($upcomingDayCount - count($tournamentDetails['upcomingDays'])));
+        $tournamentDetails['pastDays'] = $pastDays = Tournament::published()
+            ->selectRaw('DISTINCT DATE(date) as unique_date')
+            ->where('date', '<', $today)
+            ->orderBy('unique_date', 'asc')
+            ->pluck('unique_date')
+            ->take($pastDayCount);
+
+        $allTournamentDays = $pastDays->merge($tournamentDetails['upcomingDays'])->unique()->toArray();
+        $tournaments = Tournament::published()
+            ->whereIn(DB::raw('DATE(date)'), $allTournamentDays);
+        $tournaments = $tournaments->orderBy('date', 'asc');
+        $tournamentDetails['tournaments'] = $tournaments->get();
+
+        $tournamentDetails['calendarDays'] = Tournament::published()
+            ->selectRaw('DISTINCT DATE(date) as available_date')
+            ->where('date', '>=', Carbon::today()->subMonths(6)->startOfDay())
+            ->where('date', '<=', Carbon::today()->addYears(1)->endOfDay())
+            ->orderBy('available_date', 'asc')
+            ->pluck('available_date')
+            ->toArray();
+
+        return $tournamentDetails;
     }
 }
